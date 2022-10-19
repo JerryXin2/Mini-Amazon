a99bff45cc8011d075840e5f7b3b00c085c88287
Christopher Cameron <ctc41@duke.edu>
2022-10-18 21:20:07 -0400

    revert load.sql

--- db/load.sql
+++ db/load.sql
@@ -1,22 +1,11 @@
-\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
 -- since id is auto-generated; we need the next command to adjust the counter
 -- for auto-generation so next INSERT will not clash with ids loaded above:
-SELECT pg_catalog.setval('public.users_id_seq',
-                         (SELECT MAX(id)+1 FROM Users),
-                         false);
 
-\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.products_id_seq',
-                         (SELECT MAX(id)+1 FROM Products),
-                         false);
-
-\COPY Purchases FROM 'Purchases.csv' WITH DELIMITER ',' NULL '' CSV
-SELECT pg_catalog.setval('public.purchases_id_seq',
-                         (SELECT MAX(id)+1 FROM Purchases),
-                         false);
-
+\COPY Users FROM 'Users.csv' WITH DELIMITER ',' NULL '' CSV
                       
 \COPY Sellers FROM 'Sellers.csv' WITH DELIMITER ',' NULL '' CSV
+
+\COPY Products FROM 'Products.csv' WITH DELIMITER ',' NULL '' CSV
 
 \COPY Carts FROM 'Carts.csv' WITH DELIMITER ',' NULL '' CSV
 
@@ -27,3 +16,5 @@ SELECT pg_catalog.setval('public.purchases_id_seq',
 \COPY Product_Reviews FROM 'Product_Reviews.csv' WITH DELIMITER ',' NULL '' CSV
 
 \COPY Seller_Reviews FROM 'Seller_Reviews.csv' WITH DELIMITER ',' NULL '' CSV
+
+
