64c8eb50223c2f79c516468b3d2d09c0f4a1bc44
Christopher Cameron <ctc41@duke.edu>
2022-10-20 19:34:45 -0400

    Fix carts guru api

--- app/api.py
+++ app/api.py
@@ -7,6 +7,7 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 
 from .models.user import User
 from .models.cart import Cart
+from .models.cart import UserCart
 from .models.product import Product
 
 from flask import Blueprint
@@ -45,12 +46,7 @@ def carts():
     # given a user id, find the items in the cart for that user.
     form = SearchForItemsByUIDForm()
     id = form.id.data
-    if form.validate_on_submit():
-        items_in_cart = Cart.get_items_in_cart_by_uid(id)
-        return render_template('cart.html',
-                           items = items_in_cart, 
-                           form = form)
-    items_in_cart = Cart.get_items_in_cart_by_uid(id)
+    items_in_cart = UserCart.get_items_in_cart_by_uid(id)
     return render_template('cart.html',
                            items = items_in_cart, 
                            form = form)
