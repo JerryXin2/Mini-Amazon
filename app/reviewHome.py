28bd186df242c79ceb740a7e7dcbd6723e5ae337
Jerry Xin <mx47@duke.edu>
2022-12-01 22:07:53 -0500

    finished product review and seller review page

--- app/reviewHome.py
+++ app/reviewHome.py
@@ -133,16 +133,18 @@ def updateReview():
         return render_template('updateReview.html', form = form)
     return render_template('updateReview.html', form = form)
 
-class SeeSellerReviewForm(FlaskForm):
-    invalid_review = 0
-    id = IntegerField('ID of User to see Review', validators=[DataRequired()])
-    submit = SubmitField('See Review')
 
 @bp.route('/seeSellerReview', methods = ["GET", "POST"])
 def seeSellerReview():
     seller_id = request.args.get('sid')
     allSellerReviews = Seller_Review.getAllUserReview(seller_id)
     return render_template('seeSellerReview.html', avail_reviews2 = allSellerReviews)
+
+@bp.route('/seeProductReview', methods = ["GET", "POST"])
+def seeProductReview():
+    product_id = request.args.get('pid')
+    allProductReviews = Product_Review.getAllProductReview(product_id)
+    return render_template('seeProductReview.html', avail_reviews2 = allProductReviews)
     
 
 
